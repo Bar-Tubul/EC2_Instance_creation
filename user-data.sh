8e3252d4998a42576bc86cc44cb2a5731c3b22c3
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-08-29 09:27:11 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt update
-apt install -y ansible-core
+apt install -y ansible
 sudo wget https://raw.githubusercontent.com/CapSuleFor/bar-playbook/main/playbook.yml -O /home/ubuntu/playbook.yml
 ansible-playbook /home/ubuntu/playbook.yml

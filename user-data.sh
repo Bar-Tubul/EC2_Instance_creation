dc39bde8f880e5946b10a6a0bc396fdaa1f0a71f
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-09-09 19:50:10 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -4,5 +4,4 @@ apt update
 apt install -y ansible
 sudo wget https://raw.githubusercontent.com/Bar-Tubul/Ansible_automation/main/playbook.yml -O /home/ubuntu/playbook.yml
 ansible-playbook /home/ubuntu/playbook.yml
-git clone https://github.com/Bar-Tubul/Ansible-CI-CD-FWS.git
 
